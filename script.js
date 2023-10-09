1c84a93519cc14c6061c0cbc8281da036db6d4ac
matorol <mgajos00@gmail.com>
2023-10-09 18:54:11 +0200

    Adding eventListener

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("working");
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    const buttonContent = button.innerHTML;
+    console.log(buttonConte)
+
+
+    console.log('Kliknięto przycisk '+ buttonContent);
+  });
+});
